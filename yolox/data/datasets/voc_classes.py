7f366e2658802324028e12e1382ccebadd99f833
rishuKumar4you <127244009+rishuKumar4you@users.noreply.github.com>
2025-01-06 15:44:27 +0530

    Update voc_classes.py  to detect only the desired classes
    this will have only one class which is "symbols"

--- yolox/data/datasets/voc_classes.py
+++ yolox/data/datasets/voc_classes.py
@@ -3,25 +3,5 @@
 # Copyright (c) Megvii, Inc. and its affiliates.
 
 # VOC_CLASSES = ( '__background__', # always index 0
-VOC_CLASSES = (
-    "aeroplane",
-    "bicycle",
-    "bird",
-    "boat",
-    "bottle",
-    "bus",
-    "car",
-    "cat",
-    "chair",
-    "cow",
-    "diningtable",
-    "dog",
-    "horse",
-    "motorbike",
-    "person",
-    "pottedplant",
-    "sheep",
-    "sofa",
-    "train",
-    "tvmonitor",
-)
+VOC_CLASSES = ( "symbol",)
+
